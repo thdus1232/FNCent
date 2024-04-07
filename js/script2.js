b46f215008a70708f7d87dff29d20f824fcebc5f
thdus1232 <152838105+thdus1232@users.noreply.github.com>
2024-04-07 23:51:09 +0900

    rollingalbum

--- js/script2.js
+++ js/script2.js
@@ -1,19 +1,50 @@
 $(function(){ 
+
+    // let activeSubmenu;
+    // $(".nav > ul > li").hover(
+    //     function() { 
+    //         if($(this).children(".sub").is(":animated")) return;
+    //         activeSubmenu = $(this).children(".sub");
+    //         activeSubmenu.slideDown(duration);
+    //     },
+    //     function() { 
+    //         if(activeSubmenu) {
+    //             activeSubmenu.slideUp(duration);
+    //         }
+    //     }
+    // );
+
     let duration = 200;
     $(".nav > ul > li").hover(
-        // 1. #nav 요소 내의 li 요소에 마우스 커서가 들어가면
         function() { 
             if($(this).children(".sub").is(":animated")) return
-            // 2. 해당 드롭 다운 메뉴가 보이도록 한다.
             $(this).children(".sub").slideDown(duration);
         },
 
-        // 3. #nav 요소 내의 li 요소에서 마우스 커서가 나오면
         function() { 
-            // 4. 해당 드롭 다운 메뉴를 사라지도록 한다.
             $(this).children(".sub").slideUp(duration);
         }
     )
+
+    // $(".nav>ul>li").mouseover(function(){ 
+    //     $(".sub").stop().show();
+    //     $("#header").addClass("on");
+    // });
+    // $(".nav>ul>li").mouseout(function(){ 
+    //     $(".sub").stop().hide();
+    //     $("#header").removeClass("on");
+    // });
+
+    
+    // $(".nav > ul > li").hover(function () {
+    //     $(this).addClass("on");  //해당 li를 클릭하면 그 위에 ul 에게 클래스 on을 부여한다.
+    //   });
+    
+    //   $(".nav > ul > li").hover(function () {
+    //     $(this).removeClass("on");  //다른 메뉴에 hover 시에는 on 클래스를 제거한다.
+    //   });
+
+    
 
     // photo 슬라이드
     $("#photo ul li").hide().first().show();
