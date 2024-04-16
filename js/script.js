3f5f166eac4bcd9adf1f059bd69ea89981c46e54
thdus1232 <152838105+thdus1232@users.noreply.github.com>
2024-04-16 21:23:22 +0900

    almost

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ $(function() {
     clone.classList.add('clone');
     // artist banner list end
 
-
     // newrel
     let $titNames = $("#titname>li");
     let $alimg = $(".alimg");
@@ -36,62 +35,4 @@ $(function() {
         $alimg.css("display", "none")
                 .filter(targetId).css("display", "block");
     });
-
 });
-
-// 메뉴 안사라지고 그대로 있는거 원해.
-// 클릭하면 되는거 해야겠다.
-
-// $(function() { 
-//     // 0. 요소 탐색 및 변수 선언
-//     // 1. #nav 하위 요소인 a 요소를 클릭했을 때
-//     // 2. 클릭한 요소의 드롭 다운 메뉴가 숨겨진 상태면
-//     // 3. 보여지고 있는 드롭 다운 메뉴를 숨기고
-//     // 4. 클릭한 요소의 드롭 다운 메뉴를 보이도록 한다.
-//     // 5. 클릭한 요소의 드롭 다운 메뉴가 보여지는 상태이면
-//     // 6. 클릭한 요소의 드롭 다운 메뉴를 숨긴다.
-//     //------------------------------------------------------------
-//     // 0. 요소 탐색 및 변수 선언
-//     let $subMenus = $(".sub");
-//     let duration = 200;
-//     // 1. #nav 하위 요소인 a 요소를 클릭했을 때
-//     $("#nav > ul > li > a").on("click", function() { 
-//         // 클릭한 요소의 드롭 다운 메뉴
-//         let $sub = $(this).show(".sub");
-
-//         if(!$sub.length) return;
-
-//         /*
-//         // 2. 클릭한 요소의 드롭 다운 메뉴가 숨겨진 상태면
-//         if($dropdown.is(":hidden")) {
-//             // 3. 보여지고 있는 드롭 다운 메뉴를 숨기고
-//             $dropdownMenus.filter(":visible").slideUp();
-//             // 4. 클릭한 요소의 드롭 다운 메뉴를 보이도록 한다.
-//             $dropdown.slideDown();
-//         }
-//         // 5. 클릭한 요소의 드롭 다운 메뉴가 보여지는 상태이면
-//         else {
-//             // 6. 클릭한 요소의 드롭 다운 메뉴를 숨긴다.
-//             $dropdown.slideUp();
-//         }
-//         */
-
-//         if($sub.is(":hidden")) {
-//             $subMenus.filter(":visible").slideUp(duration);
-//         }
-        
-//         $sub.slideToggle(duration);
-
-//     });
-// });
-// var bigPic = document.querySelector(".big");
-// var smallPics = document.querySelectorAll(".small");
-
-// for(var i = 0 ; i < smallPics.length; i++ ){
-//     smallPics[i].addEventListener("mouseover", changepic);
-// }
-// function changepic(){
-//     var smallPicAttribute = this.getAttribute("src");
-//     bigPic.setAttribute("src", smallPicAttribute);
-
-// }
--- js/script2.js
+++ js/script2.js
@@ -1,57 +1,25 @@
 $(function(){ 
-
-    // let activeSubmenu;
-    // $(".nav > ul > li").hover(
-    //     function() { 
-    //         if($(this).children(".sub").is(":animated")) return;
-    //         activeSubmenu = $(this).children(".sub");
-    //         activeSubmenu.slideDown(duration);
-    //     },
-    //     function() { 
-    //         if(activeSubmenu) {
-    //             activeSubmenu.slideUp(duration);
-    //         }
-    //     }
-    // );
-
     let duration = 200;
+    let activeSubMenu;
+    
     $(".nav > ul > li").hover(
-        function() { 
-            if($(this).children(".sub").is(":animated")) return
-            $(this).children(".sub").slideDown(duration);
+        function() {
+            if($(this).children(".sub").is(":animated")) return;
+            if(activeSubMenu) activeSubMenu.stop().slideUp(duration);
+            activeSubMenu = $(this).children(".sub");
+            activeSubMenu.stop().slideDown(duration);
         },
-
-        function() { 
-            $(this).children(".sub").slideUp(duration);
+        function() {
+            activeSubMenu.stop().slideDown(duration);
         }
-    )
-
-    // $(".nav>ul>li").mouseover(function(){ 
-    //     $(".sub").stop().show();
-    //     $("#header").addClass("on");
-    // });
-    // $(".nav>ul>li").mouseout(function(){ 
-    //     $(".sub").stop().hide();
-    //     $("#header").removeClass("on");
-    // });
-
-    
-    // $(".nav > ul > li").hover(function () {
-    //     $(this).addClass("on");  //해당 li를 클릭하면 그 위에 ul 에게 클래스 on을 부여한다.
-    //   });
-    
-    //   $(".nav > ul > li").hover(function () {
-    //     $(this).removeClass("on");  //다른 메뉴에 hover 시에는 on 클래스를 제거한다.
-    //   });
-
-    
+    );
 
     // photo 슬라이드
     $("#photo ul li").hide().first().show();
-    var i=0;
+    let i = 0;
     function slide (){
         if(i<2)i++;
-        else i=0;
+        else i = 0;
         $("#photo ul li").fadeOut(1200);
         $("#photo ul li").eq(i).fadeIn(1200);
     };
